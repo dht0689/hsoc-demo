f2bb7c6ee62a46db6f2b16dbdfbc3df2e682194c
dunghoangtrong <dunghoangtrong@gmail.com>
2020-05-23 01:54:58 +0700

    Update hsoc.bat

--- hsoc.bat
+++ hsoc.bat
@@ -1,2 +1,3 @@
-sc.exe create hsoc-service binPath=%temp%\service.exe
-sc.exe start hsoc-service
+::sc.exe create hsoc-service binPath=%temp%\service.exe
+::sc.exe start hsoc-service
+echo "this pc is hacked"
